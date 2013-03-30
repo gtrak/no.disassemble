f8f01509bc196f60014afe4e8967931166ca71ec
Gary Trakhman <gary.trakhman@gmail.com>
2013-03-30 00:29:13 -0400

    task stub

--- lein-nodisassemble/src/leiningen/nodisassemble.clj
+++ lein-nodisassemble/src/leiningen/nodisassemble.clj
@@ -1,14 +1,7 @@
-(ns leiningen.nodisassemble
-  (:require [leiningen.util.deps :as deps]))
+(ns leiningen.nodisassemble)
 
-
-(defn middleware [project]
-  (let [project (update-in project [:dependencies] conj '[nodisassemble "0.1.0-SNAPSHOT"])
-        file (deps/dependency-hierarchy project)]
-    (println file)
-    (update-in project [:jvm-opts] identity)))
-
-(defn nodisassemble
-  "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+(comment
+  (defn nodisassemble
+    "I don't do a lot."
+    [project & args]
+    (println "Hi!")))
